c90aa471bc99015d9ca5c1221356ec5ab511204a
Aletschhorn <99950173+Aletschhorn@users.noreply.github.com>
2022-10-16 16:17:57 +0200

    Fixes #2

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+// only used in Typo3 version 11 and higher
+return [
+   'fku-slider' => [
+      'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+      'source' => 'EXT:fku_slider/Resources/Public/Icons/Extension.svg',
+   ],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -12,6 +12,12 @@ defined('TYPO3_MODE') or die();
 	[]
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fku_slider/Configuration/TypoScript/contentElementWizard_TsConfig.txt">'
+// icon registration for Typo3 version 10 (not used for version 11 anymore)
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$iconRegistry->registerIcon(
+   'fku-slider',
+   \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+   ['source' => 'EXT:fku_slider/Resources/Public/Icons/Extension.svg']
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:fku_slider/Configuration/TsConfig/contentElementWizard.tsconfig"');
